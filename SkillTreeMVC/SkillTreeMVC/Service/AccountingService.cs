7e72b77ac402bc106aa0b6e555a84733536db94c
YiLung Chen <lung7817@icloud.com>
2019-07-18 22:41:34 +0800

    新增AccountingService

---
+++ SkillTreeMVC/SkillTreeMVC/Service/AccountingService.cs
@@ -0,0 +1,30 @@
+﻿using SkillTreeMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkillTreeMVC.Service
+{
+    
+    public class AccountingService
+    {
+        private readonly SkillTreeHomeworkEntities _db;
+        public AccountingService()
+        {
+            _db = new SkillTreeHomeworkEntities();
+        }
+
+        public IEnumerable<AccountingViewModel> Lookup()
+        {
+            var accountingList = _db.AccountBook.OrderByDescending(d => d.Dateee).Select(d => new AccountingViewModel()
+            {
+                Type = d.Categoryyy.ToString(),
+                Amount = d.Amounttt.ToString(),
+                Date = d.Dateee,
+                Note = d.Remarkkk
+            });
+            return accountingList;
+        }
+    }
+}
