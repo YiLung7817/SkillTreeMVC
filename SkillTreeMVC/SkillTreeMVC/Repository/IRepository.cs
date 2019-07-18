298d917f09d4c5db7517b26491067137eaf433c5
YiLung Chen <lung7817@icloud.com>
2019-07-18 23:04:25 +0800

    新增Repository與IRepository

---
+++ SkillTreeMVC/SkillTreeMVC/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTreeMVC.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> LookupAll();
+
+        IQueryable<T> Query(Expression<Func<T, bool>> filter);
+
+
+    }
+}
---
+++ SkillTreeMVC/SkillTreeMVC/Repository/Repository.cs
@@ -0,0 +1,27 @@
+﻿using SkillTreeMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace SkillTreeMVC.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _dbContext;
+        public Repository() {
+            _dbContext = new SkillTreeHomeworkEntities();
+        }
+        public IQueryable<T> LookupAll()
+        {
+            return _dbContext.Set<T>();
+        }
+
+        public IQueryable<T> Query(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
