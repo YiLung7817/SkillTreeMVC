0d0b179b179963f6f5e3b2419cd105f9150a3f2a
YiLung Chen <lung7817@icloud.com>
2019-07-18 23:38:39 +0800

    加入UnitOfWork與IUnitOfWork

---
+++ SkillTreeMVC/SkillTreeMVC/Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using SkillTreeMVC.Models;
+
+namespace SkillTreeMVC.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public DbContext Context { get; set; }
+
+        public UnitOfWork()
+        {
+            Context = new SkillTreeHomeworkEntities();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
