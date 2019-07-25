d999f77701e3f9f336281d06c0c966ef4e7af75c
YiLung Chen <lung7817@icloud.com>
2019-07-25 23:38:37 +0800

    新增viewModel資料驗證

--- SkillTreeMVC/SkillTreeMVC/Controllers/HomeController.cs
+++ SkillTreeMVC/SkillTreeMVC/Controllers/HomeController.cs
@@ -21,7 +21,6 @@ namespace SkillTreeMVC.Controllers
 
         public ActionResult Index()
         {
-            
             return View();
         }
 
@@ -42,6 +41,36 @@ namespace SkillTreeMVC.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        /// <summary>
+        /// 驗證日期不可大於今天
+        /// </summary>
+        /// <param name="date">日期</param>
+        /// <returns></returns>
+        public ActionResult CheckDateLessThanToday(DateTime? date)
+        {
+            var isValidate = false;
+            if (date != null)
+            {
+                isValidate = date <= DateTime.Now;
+            }
+            return Json(isValidate, JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>
+        /// 驗證金額是否為正整數
+        /// </summary>
+        /// <param name="amount">金額</param>
+        /// <returns></returns>
+        public ActionResult CheckInt(string amount)
+        {
+            if (amount.Length >= 2 && amount.IndexOf("0") == 0)
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(int.TryParse(amount, out int result), JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
--- SkillTreeMVC/SkillTreeMVC/Models/AccountViewModel.cs
+++ SkillTreeMVC/SkillTreeMVC/Models/AccountViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace SkillTreeMVC.Models
 {
@@ -17,18 +19,30 @@ namespace SkillTreeMVC.Models
         /// <summary>
         /// 類別
         /// </summary>
+        [Required]
+        [Display(Name = "類別")]
         public string Category { get; set; }
         /// <summary>
         /// 金額
         /// </summary>
+        [Required]
+        [Display(Name = "金額")]
+        [Range(0, int.MaxValue)]
+        [Remote("CheckInt", "Home", ErrorMessage = "tttt")]
         public int Amount { get; set; }
         /// <summary>
         /// 日期
         /// </summary>
+        [Required]
+        [Display(Name = "日期")]
+        [Remote("CheckDateLessThanToday", "Home", ErrorMessage = "日期不可大於今天")]
         public DateTime Date { get; set; }
         /// <summary>
         /// 備註
         /// </summary>
+        [Required]
+        [Display(Name = "備註")]
+        [StringLength(100)]
         public string Note { get; set; }
     }
 }
