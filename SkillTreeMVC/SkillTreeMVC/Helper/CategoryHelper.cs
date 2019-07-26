c3bc6083c3ff64ac7f2d5141d3035e070b22ed84
YiLung Chen <lung7817@icloud.com>
2019-07-26 21:51:36 +0800

    新增CategoryHelper加入支出及收入顯示邏輯

---
+++ SkillTreeMVC/SkillTreeMVC/Helper/CategoryHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkillTreeMVC.Helper
+{
+    public static class CategoryHelper
+    {
+        public static MvcHtmlString DisplayCategory(this HtmlHelper helper, int category)
+        {
+
+            if (category == 0)
+            {
+                return new MvcHtmlString("<span class='text-danger'>支出</span>");
+            }
+            return new MvcHtmlString("<span class='text-primary'>收入</span>");
+        }
+    }
+    
+}
